6a7c5557d008e005e73e90c672fa35192645b1ab
ciric623.admin <srdjanciric623@gmail.com>
2024-04-09 21:55:05 +0200

    ledInit function added

--- LED_Blink/main.c
+++ LED_Blink/main.c
@@ -1,17 +1,26 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdint.h>
-int16_t main ()
-{
-int16_t high_time = 300;
-int16_t low_time = 700;
-DDRB |= 1 << 5; // PB5 je izlaz
-while (1)
-{
-PORTB |= 1 << 5; // LED ON
-_delay_ms ( high_time ) ; // Pauza 1 s
-PORTB &= ~(1 << 5) ; // LED OFF
-_delay_ms ( low_time ) ; // Pauza 1 s
+void ledBlink(int16_t high_time, int16_t low_time) {
+    PORTB |= 1 << 5; // LED ON
+    _delay_ms(high_time); // Pauza high_time ms
+    PORTB &= ~(1 << 5); // LED OFF
+    _delay_ms(low_time); // Pauza low_time ms
 }
-return 0;
+
+void ledInit() {
+    DDRB |= 1 << 5; // PB5 je izlaz
+}
+
+int16_t main () {
+    int16_t high_time = 300; // Promenjena vrednost high_time na 300
+    int16_t low_time = 700; // Promenjena vrednost low_time na 700
+    
+    ledInit(); // Poziv funkcije ledInit() za inicijalizaciju LED diode
+    
+    while (1) {
+        ledBlink(high_time, low_time); // Poziv funkcije ledBlink() za blinkanje LED diode
+    }
+    
+    return 0;
 }
